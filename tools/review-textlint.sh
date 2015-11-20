537c192f717027c797120efa0ee5252f8928b68a
azu <azuciao@gmail.com>
2015-11-20 23:11:21 +0900

    chore(tool): use circle ci env

--- tools/review-textlint.sh
+++ tools/review-textlint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-  gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
-
-  diffBranchName="origin/master"
-  # 変更行のみを対象にする
-  git diff --name-only --diff-filter=ACMR ${diffBranchName} \
-  | grep -a '_posts/.*.md$' \
-  | xargs $(npm bin)/textlint -f checkstyle \
-  | checkstyle_filter-git diff ${diffBranchName} \
-  | saddler report \
-      --require saddler/reporter/github \
-      --reporter Saddler::Reporter::Github::PullRequestReviewComment
+if [ "$CI_PULL_REQUEST" == false ] || [ -z "$CI_PULL_REQUEST" ]; then
+  echo 'not pull request.'
+  exit 0
 fi
-exit 0
+
+gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
+
+diffBranchName="origin/master"
+# 変更行のみを対象にする
+git diff --name-only --diff-filter=ACMR ${diffBranchName} \
+| grep -a '_posts/.*.md$' \
+| xargs $(npm bin)/textlint -f checkstyle \
+| checkstyle_filter-git diff ${diffBranchName} \
+| saddler report \
+    --require saddler/reporter/github \
+    --reporter Saddler::Reporter::Github::PullRequestReviewComment
